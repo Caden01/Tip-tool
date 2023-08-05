f0fd2db3f9858d24d2db9de84d384c12e612679a
Caden01 <caden.carlson01@gmail.com>
2023-08-05 10:50:59 -0700

    wrote tests for each function in server.js and cleaned up the DOM after each test is run

--- servers.test.js
+++ servers.test.js
@@ -1,17 +1,33 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
-    serverNameInput.value = 'Alice';
+    serverNameInput.value = "Alice";
   });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
+  it("should add a new server to allServers on submitServerInfo()", function () {
     submitServerInfo();
 
     expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    expect(allServers["server" + serverId].serverName).toEqual("Alice");
   });
 
-  afterEach(function() {
-    // teardown logic
+  it("should not add a new server when submit ServerInfo() is called with an empty input", () => {
+    serverNameInput.value = "";
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it("should upadte the #serverTable when updateServerTable() is called", () => {
+    submitServerInfo();
+    updateServerTable();
+    let list = document.querySelectorAll("#serverTable tbody tr td");
+
+    expect(list[0].innerText).toEqual("Alice");
+  });
+
+  afterEach(function () {
+    serverId = 0;
+    serverTbody.innerHTML = "";
+    allServers = {};
   });
 });
